94593c222bfaa747a1fccf62f8786a9a4d8c0728
Sandesh Kumar <sandeshkumar09123@gmail.com>
2026-07-24 17:34:49 +0530

    guessgameV2

--- guess number.py
+++ guess number.py
@@ -4,34 +4,41 @@ number=7
 attempt=0
 
 while True:
-  guess_the_number=int(input('enter your number in range 0-10 :'))
+  choice=input("play agin? yes/no: ")
+
+  if choice=='yes':
+    while True:
+      guess_the_number=int(input('enter your number in range 0-10 :'))
   
 
 
-  if not (0 <= guess_the_number <= 10):
-    print('enter the guess_the_number in between range 0 to 10')
-    continue
+      if not (0 <= guess_the_number <= 10):
+         print('enter the guess_the_number in between range 0 to 10')
+         continue
 
-  attempt+=1
+      attempt+=1
 
 
 
-  if guess_the_number==number:
-    print('you guess right')
-    break
+      if guess_the_number==number:
+        print('you guess right')
+        break
     
     
-  elif guess_the_number<number:
-    print('your guess number is lower guess higher')
+      elif guess_the_number<number:
+        print('your guess number is lower guess higher')
     
     
 
-  elif guess_the_number>number:
-    print('your guess is higher guess lower')
+      elif guess_the_number>number:
+         print('your guess is higher guess lower')
     
     
-  else:
-    pass
+      else:
+        pass
   
-print(f'You guessed the number in {attempt} attempts.')
-
+    print(f'You guessed the number in {attempt} attempts.')
+  
+  elif choice=='no':
+    break         
+        
